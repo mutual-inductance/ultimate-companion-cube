ba509deac9905ede97921004ca63669fdc40b6f5
mutual-inductance <spreadsheetdragon@proton.me>
2026-01-24 00:20:59 -0500

    Update code.py
    Updated with functional responding touch loop

--- code.py
+++ code.py
@@ -1,51 +1,35 @@
-# Ultimate Companion Cube
-# Built for Myrtarous by Mutual_Inductance
-
-# libraries and modules
 import board
 import digitalio
 import time
 import touchio  # for capacitive touch sensor
 from adafruit_debouncer import Debouncer  # for capacitive touch sensor
-from rainbowio import colorwheel  # for neopixel output
 import neopixel  # for neopixel output
 from os import getenv  # for wifi
-import ipaddress  # for wifi
-import wifi  # for wifi
-import socketpool  # for wifi
-import adafruit_connection_manager
 import adafruit_requests
-import microcontroller
-from adafruit_io.adafruit_io import IO_HTTP, AdafruitIO_RequestError
+import wifi
+import ipaddress  # for wifi
+import adafruit_connection_manager
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
+from adafruit_io.adafruit_io import IO_MQTT
+from rainbowio import colorwheel
 
-# WIFI
-#  Get WiFi details, ensure these are setup in settings.toml
-ssid = getenv("CIRCUITPY_WIFI_SSID")
-password = getenv("CIRCUITPY_WIFI_PASSWORD")
+wifi_ssid = getenv("CIRCUITPY_WIFI_SSID")
+wifi_password = getenv("CIRCUITPY_WIFI_PASSWORD")
 aio_username = getenv("ADAFRUIT_AIO_USERNAME")
 aio_key = getenv("ADAFRUIT_AIO_KEY")
 
-if None in [ssid, password]:
-    raise RuntimeError(
-        "WiFi settings are kept in settings.toml, "
-        "please add them there. The settings file must contain "
-        "'CIRCUITPY_WIFI_SSID', 'CIRCUITPY_WIFI_PASSWORD', "
-        "at a minimum."
-    )
-print()
-print("Connecting to WiFi")
+radio = wifi.radio
+while not radio.connected:
+    radio.connect(wifi_ssid, wifi_password)
 
-#  connect to your SSID
-try:
-    wifi.radio.connect(ssid, password)
-except TypeError:
-    print("Could not find WiFi info. Check your settings.toml file!")
-    raise
-print("Connected to WiFi")
+# get the pool and ssl_context from the helpers:
+pool = adafruit_connection_manager.get_radio_socketpool(radio)
+ssl_context = adafruit_connection_manager.get_radio_ssl_context(radio)
 
-pool = socketpool.SocketPool(wifi.radio)
+# request session
+requests = adafruit_requests.Session(pool, ssl_context)
+connection_manager = adafruit_connection_manager.get_connection_manager(pool)
 
-#  prints MAC address to REPL
 print("My MAC addr:", [hex(i) for i in wifi.radio.mac_address])
 
 #  prints IP address to REPL
@@ -55,45 +39,79 @@ print(f"My IP address is {wifi.radio.ipv4_address}")
 ipv4 = ipaddress.ip_address("8.8.4.4")
 print("Ping google.com: %f ms" % (wifi.radio.ping(ipv4) * 1000))
 
-# ADAFRUIT IO
-pool = adafruit_connection_manager.get_radio_socketpool(wifi.radio)
-ssl_context = adafruit_connection_manager.get_radio_ssl_context(wifi.radio)
-requests = adafruit_requests.Session(pool, ssl_context)
+# MQTT Setup ###
 
-# Initialize an Adafruit IO HTTP API object
-io = IO_HTTP(aio_username, aio_key, requests)
+mqtt_TX = f"{aio_username}/feeds/cc1"
+mqtt_RX = f"{aio_username}/feeds/cc0"
 
-light_on = 1
-light_off = 0
-try:
-    # Get the 'light' feed from Adafruit IO
-    light_feed = io.get_feed("light")
-except AdafruitIO_RequestError:
-    # If no 'light' feed exists, create one
-    light_feed = io.create_new_feed("light")
+# Define callback methods which are called when events occur
+def connected(client):
+    # This function will be called when the mqtt_client is connected
+    # successfully to the broker.
+    print("Connected to AdafruitIO!")
+    client.subscribe("cc0")
 
+def subscribe(client, userdata, topic, granted_qos):
+    # This method is called when the mqtt_client subscribes to a new feed.
+    print(f"Subscribed to {topic}")
+
+
+def publish(client, userdata, topic, pid):
+    # This method is called when the mqtt_client publishes data to a feed.
+    print(f"Published to {topic}")
+
+
+def message(client, feedid, msg):
+    print(f"New message from {client} on {feedid}: {msg}")
+    if msg == "here":
+        pixels.fill(ROSE)
+        pixels.brightness = .8
+        pixels.show()
+        for i in range(10):
+            touch_deb.update()        
+            if touch_deb.rose:
+                io.publish('cc1', 'together')
+                lightcycle()
+            else: 
+                time.sleep(.5)
+    elif msg == "together":
+        lightcycle()
+    elif msg == "off":
+        return 'off'
+    else:
+        print(f"Unexpected message on {feedid} feed.")
+
+# Set up a MiniMQTT Client
+mqtt_client = MQTT.MQTT(
+    broker="io.adafruit.com",
+    port=1883,
+    username=aio_username,
+    password=aio_key,
+    socket_pool=pool,
+    ssl_context=ssl_context,
+    )
+    
+io = IO_MQTT(mqtt_client)
+
+# Connect callback handlers to mqtt_client
+io.on_connect = connected
+io.on_subscribe = subscribe
+io.on_publish = publish
+io.on_message = message
+
+print(f"Attempting to connect to {mqtt_client.broker}")
+io.connect()
+
+print(f"Publishing to {mqtt_TX}")
+io.publish('cc1', 'off')
 
 # LIGHTS
 pixel_pin = board.GP6
 num_pixels = 8
+speed = .05
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.3, auto_write=False, pixel_order=(1, 0, 2, 3)
 )
-
-
-def colorwheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        return (0, 0, 0, 0)
-    if pos < 85:
-        return (255 - pos * 3, pos * 3, 0, 0)
-    if pos < 170:
-        pos -= 85
-        return (0, 255 - pos * 3, pos * 3, 0)
-    pos -= 170
-    return (pos * 3, 0, 255 - pos * 3, 0)
-
 
 def color_chase(color, wait):
     for i in range(num_pixels):
@@ -103,15 +121,64 @@ def color_chase(color, wait):
         print(color)
     time.sleep(0.5)
 
-
 def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            rc_index = (i * 256 // num_pixels) + j
-            pixels[i] = colorwheel(rc_index & 255)
+    for color in range(255):
+        for pixel in range(len(pixels)):
+            pixel_index = (pixel * 256 // len(pixels)) + color * 5
+            pixels[pixel] = colorwheel(pixel_index & 255)
         pixels.show()
         time.sleep(wait)
 
+def lightcycle():
+    pixels.brightness = .5
+    pixels.fill(ROSE)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(PURPLE)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(IRIS)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(MAGENTA)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(PINK)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(FUSCHIA)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(CORAL)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(LIGHTRED)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(ROSE)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(PURPLE)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(IRIS)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(MAGENTA)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(PINK)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(FUSCHIA)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(CORAL)
+    pixels.show()
+    time.sleep(.5)
+    pixels.fill(LIGHTRED)
+    pixels.show()
+    time.sleep(.5) 
 
 WHITE = (255, 255, 255, 255)
 RED = (255, 0, 0, 0)
@@ -133,58 +200,23 @@ LIGHTRED = (255, 0, 53, 0)
 touch_pad = touchio.TouchIn(board.GP19, digitalio.Pull.UP)
 touch_deb = Debouncer(touch_pad)
 
+wait_duration = 10
+
 # main loop
 while True:
-    # Get data from 'digital' feed
-    print("getting data from IO...")
-    feed_data = io.receive_data(light_feed["key"]) 
-    
-    # Check if data is ON or OFF
-    if int(feed_data["value"]) == 1:
-        print("received <- ON\n")
-        pixels.fill(ROSE)
-        pixels.show()
-        time.sleep(5)  # will this work if I need it to be listening for a touch on the light_feed? 
-    elif int(feed_data["value"]) == 0:
-        print("received <= OFF\n")
-
+    # receive messages from MQTT feed
+    io.loop()
     # check for touch
     touch_deb.update()
-    
     if touch_deb.rose:  # using rose to sense only when someone touches the lamp.
+        io.publish('cc1', 'here')
         pixels.fill(ROSE)
+        pixels.brightness = .8
         pixels.show()
-        print("sending to adafruitIO")
-        io.send_data(light_feed["key"], light_on)
-        time.sleep(5)
-        io.send_data(light_feed["key"], light_off)
-    # Increase or decrease to change the speed of the solid color change.
-    #    time.sleep(5)
-    #    pixels.fill(MAGENTA)
-    #    pixels.show()
-    #    time.sleep(5)
+        io.loop(5)
+        io.publish('cc1', 'off')
     else:
         pixels.fill(WHITE)
+        pixels.brightness = .1
         pixels.show()
         time.sleep(.2)
-#    color_chase(PURPLE, .08)
-#    color_chase(MAGENTA, .08)
-#    color_chase(IRIS, .08)
-#    color_chase(FUSCHIA, .08)
-#    color_chase(CORAL, .08)
-#    color_chase(ROSE, .08)
-#    color_chase(LIGHTRED, .08)
-#    color_chase(RED, .08)
-#    color_chase(LIGHTRED, .08)
-#    color_chase(ROSE, .08)
-#    color_chase(CORAL, .08)
-#    color_chase(FUSCHIA, .08)
-#    color_chase(IRIS, .08)
-#    color_chase(MAGENTA, .08)
-# Increase the number to slow down the color chase
-#    color_chase(YELLOW, .08)
-#    color_chase(GREEN, .08)
-#    color_chase(CYAN, .08)
-#    color_chase(BLUE, .08)
-
-#    rainbow_cycle(0)  # Increase the number to slow down the rainbow
